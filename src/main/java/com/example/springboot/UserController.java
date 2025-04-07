e6b6cd2880d38a4cd8bd7da616cebe16c9779892
Bofeng Li <bofeng.li@outlook.com>
2025-04-08 00:20:05 +0200

    User AI generate API for reading user.

---
+++ src/main/java/com/example/springboot/UserController.java
@@ -0,0 +1,26 @@
+package com.example.springboot;
+
+import com.example.springboot.entity.User;
+import com.example.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @GetMapping("/{login}")
+    public User getUserByLogin(@PathVariable String login) {
+        return userRepository.findByLogin(login);
+    }
+}
